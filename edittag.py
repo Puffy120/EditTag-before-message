76d34adbe4088b1476236f77f93f97d1cad4be51
Puffy <53114830+Puffy120@users.noreply.github.com>
2021-01-23 15:30:44 +0100

    Add files via upload

---
+++ edittag.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+prefix = "*" #you can change the prefix here
+bot = discord.Client()
+bot = commands.Bot(command_prefix = prefix)
+MAGIC_CHAR = '\u202b'
+
+@bot.event
+async def on_ready():
+    print("it's ready")
+    
+@bot.command()
+async def editglitch(ctx, message):
+	await ctx.message.delete()
+	msg = await ctx.send(message)
+	await msg.edit(content=f"{MAGIC_CHAR} {message} {MAGIC_CHAR}")
+
+                       
+bot.run("ur gay token", bot=False)
